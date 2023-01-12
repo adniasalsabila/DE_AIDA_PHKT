658f7bd80952ef040db43180e0de93ce936175cb
adniasalsabila <adniasalsabila@student.telkomuniversity.ac.id>
2023-01-12 07:24:52 +0700

    login and dashboard commit

---
+++ app/src/main/java/com/project/aidaphkt/View/DashboardActivity.kt
@@ -0,0 +1,13 @@
+package com.example.aidaphkt.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.aidaphkt.R
+
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
---
+++ app/src/main/java/com/project/aidaphkt/View/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.project.aidaphkt.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.aidaphkt.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
